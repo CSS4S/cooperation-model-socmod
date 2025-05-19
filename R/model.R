1172515db5863549a382ebd3a5d7f14bbfe8646a
Matt Turner <maturner01@gmail.com>
2025-05-19 08:19:06 -0700

    first working model

--- R/model.R
+++ R/model.R
@@ -1,10 +1,10 @@
 library(igraph)
 library(socmod)
 
-make_cooperation_model <- function(grid_height, grid_width, coop_benefit,
-                                   coop_cost) {
+make_cooperation_model <- function(grid_height = 11, grid_width = 11, 
+                                   coop_benefit = 1.0, coop_cost = 0.25) {
   
-  g <- make_lattice(dim = c(grid_height, grid_width), circular = FALSE)
+  g <- make_lattice(dimvector = c(grid_height, grid_width), periodic = FALSE)
   
   # Initialized like Smaldino _MSB_ Ch. 6
   abm <- 
@@ -31,7 +31,7 @@ play_game <- function(focal_agent, partner_agent, model) {
       return (model$coop_benefit - model$coop_cost)
     # Focal agent is betrayed
     } else {
-      return (- model$coop_cost)
+      return (-1.0 * model$coop_cost)
     }
   } else {
     # Focal agent defects, gets cooperative benefit
@@ -45,11 +45,46 @@ play_game_with_neighbors <- function(focal_agent, model) {
   
   # Focal agent plays game with neighbors, accumulating payoffs
   total_payoff <- 
-    sum(
+    sum(unlist(
       focal_agent$get_neighbors()$map(\(n) play_game(focal_agent, n, model))
-    )
+    ))
   
   focal_agent$set_next_fitness(total_payoff)
+}
+
+
+coop_model_step <- function(abm) {
+  # Each agent plays game, receiving a total fitness from playing with each neighbor
+  purrr::walk(
+    abm$agents,
+    \(agent) {
+      play_game_with_neighbors(agent, abm)
+    }
+  )
+  
+  # After each agent plays, iterate through again to do success-biased learning
+  purrr::walk(
+    abm$agents,
+    \(agent) {
+      # print(agent$get_id())
+      # print(purrr::map_vec(agent$get_neighbors()$agents, \(a) a$get_id()))
+      neighbors <- agent$get_neighbors()
+      tot_neighbor_fitnesses <- 
+        sum(unlist(neighbors$map(\(n) n$get_fitness())))
+      if (tot_neighbor_fitnesses == 0.0) {
+        teacher <- neighbors$sample()
+      } else {
+        teacher <- success_bias_select_teacher(agent, abm)
+      }
+      agent$set_next_behavior(teacher$get_behavior())
+      
+      # Agent-level fitness resets after each round (i.e. time step in this case)
+      agent$set_next_fitness(0.0)
+    }
+  )
+  
+  # Use socmod-provided model step function for learning given next_behavior/fitness
+  iterate_learning_model(abm)
 }
 
 
@@ -59,21 +94,6 @@ play_game_with_neighbors <- function(focal_agent, model) {
 normal_game_strategy <- make_learning_strategy(
   partner_selection = \(f, m) NULL,
   interaction = \(f, p, m) NULL,
-  model_step = \(abm) coop_model_step,
+  model_step = coop_model_step,
   label = "Normal game strategy"
 )
-
-
-coop_model_step <- function(abm) {
-  # Each agent plays game, receiving a total fitness from playing with each neighbor
-  purrr::walk(
-    abm$agents,
-    \(agent) {
-      play_game_with_neighbors(agent, abm)
-      
-      # Use success-biased learning
-    }
-  )
-  
-  
-}
